4cdd758bff992aaea9ec76d55fa08f24e98ff6ef
Luiyit <luiyit.hernandez@gmail.com>
2025-12-04 10:22:54 +0100

    Extend model types

--- src/templates/modelTypes.template.ts
+++ src/templates/modelTypes.template.ts
@@ -1,9 +1,17 @@
 export const MODEL_TYPES_TEMPLATE =`
-import { Prisma, #!{MODEL_NAME} } from '@prisma/client';
+import { Prisma, #!{MODEL_NAME} } from "@prisma/client";
 
-type New#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}CreateArgs['data'];
-type update#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}UpdateArgs['data'];
+type New#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}CreateArgs["data"];
+type update#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}UpdateArgs["data"];
+type FindFirst#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}FindFirstArgs["where"];
+type FindMany#!{MODEL_NAME} = Prisma.#!{MODEL_NAME}FindManyArgs["where"];
 
-export { type #!{MODEL_NAME}, type New#!{MODEL_NAME}, type update#!{MODEL_NAME} };
+export { 
+    type #!{MODEL_NAME}, 
+    type New#!{MODEL_NAME}, 
+    type update#!{MODEL_NAME}, 
+    type FindOne#!{MODEL_NAME}, 
+    type FindMany#!{MODEL_NAME},
+};
 
 `;
